6a049cccf07d7f67225c8e881d3fde7b0d3a5d97
Kriukov Sergey <krus-83@mail.ru>
2017-11-01 16:07:37 +0700

    step-2 correction of remarks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greet = () => {
-	const actual = readlineSync.question('May I have your name?: ');
+	const userName = readlineSync.question('May I have your name?: ');
 
-  console.log('Hello, ' + actual + '!');
+  console.log(`Hello, ${userName}!`);
 }
 
 export default greet;
